ed3d12ca5854dea256d2894a57b63a2f1d6c7ec9
jitendra4545 <jitendra45.js45@gmail.com>
2023-04-29 22:57:23 +0530

    db connected and folderStructure completed

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const { connection } = require('./config/db')
+const app = express()
 require('dotenv').config()
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send(`welcome back`)
 })
 
+// server will run at port ****** ----------------------------------------->
 
-
-app.listen(process.env.port,()=>{
+app.listen(process.env.port, async () => {
+    try{
+     await connection
+     console.log(`DB connected Successfully`)
+    }catch(err){
+    console.log('unable to connect with DB',err)
+    }
     console.log(`server running on port ${process.env.port}`)
 })
